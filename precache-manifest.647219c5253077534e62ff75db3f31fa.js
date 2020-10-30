b6eaf6c1b80594f22c19da4f40c5d2d5bf874325
Olena kuzii <okuziy@gmail.com>
2020-10-30 11:23:00 +0200

    Updates

---
+++ precache-manifest.647219c5253077534e62ff75db3f31fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5850c7012c85f605ffcd1466ad835694",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44af821d4cf94cbae050",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "8d6acd2d19c22d1ae1a9",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "44af821d4cf94cbae050",
+    "url": "./static/js/2.d10b847f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d10b847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6acd2d19c22d1ae1a9",
+    "url": "./static/js/main.2b0005ae.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
